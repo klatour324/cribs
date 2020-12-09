a9f4e0e6853d1f61cb883228ea3f54898d37619a
Katy La Tour <klatour324@gmail.com>
2020-12-09 14:39:52 -0600

    Create test to add rooms

--- test/house_test.rb
+++ test/house_test.rb
@@ -16,17 +16,15 @@ class HouseTest < Minitest::Test
 
     assert_equal [], house.rooms
   end
+
+  def test_it_can_add_rooms
+    house = House.new("$400000", "123 sugar lane")
+    room_1 = Room.new(:bedroom, 10, '13')
+    room_2 = Room.new(:bedroom, 11, '15')
+
+     house.add_room(room_2)
+     house.add_room(room_1)
+     
+     assert_equal [room_2, room_1], house.rooms
+  end
 end
-#
-#  room_1 = Room.new(:bedroom, 10, '13')
-# #=> #<Room:0x00007fccd29b5720...>
-#
-#  room_2 = Room.new(:bedroom, 11, '15')
-# #=> #<Room:0x00007fccd2985f48...>
-#
-#  house.add_room(room_1)
-#
-#  house.add_room(room_2)
-#
-#  house.rooms
-# #=> [#<Room:0x00007fccd29b5720...>, #<Room:0x00007fccd2985f48...>]
