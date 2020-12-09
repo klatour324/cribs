7e3185ca8714f3bfea7a088f8ea1019b8d664dc1
Katy La Tour <klatour324@gmail.com>
2020-12-09 15:47:13 -0600

    create test for details of house

--- test/house_test.rb
+++ test/house_test.rb
@@ -60,11 +60,11 @@ class HouseTest < Minitest::Test
     assert_equal 1900, house.area
   end
 
-  # def test_it_has_details
-  #   house = House.new("$400000", "123 sugar lane")
-  #
-  #   house_details = {"price" => 400000, "address" => "123 sugar lane"}
-  #
-  #   assert_equal house_details, house.details
-  # end
+  def test_it_has_details
+    house = House.new("$400000", "123 sugar lane")
+
+    house_details = {"price" => 400000, "address" => "123 sugar lane"}
+
+    assert_equal house_details, house.details
+  end
 end
